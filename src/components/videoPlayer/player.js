02dae331931ef905ae28931268ff80f9c4b20c13
Dave <daviti.ivanelashvili@gmail.com>
2022-10-24 21:37:42 +0400

    firs

---
+++ src/components/videoPlayer/player.js
@@ -0,0 +1,15 @@
+import ReactPlayer from 'react-player'
+
+const Player=()=>{
+    return(
+        <div>
+            <ReactPlayer
+            url='https://www.youtube.com/watch?v=UDVtMYqUAyw'
+            width='100%'
+            height='100vh'
+            />
+        </div>
+    )
+
+}
+export default Player
